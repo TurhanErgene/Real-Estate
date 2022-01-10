385a4032a3e0220c8324b4227d06f13ba9a15d74
Turhan <turhan-ergene@hotmail.com>
2022-01-11 00:37:58 +0100

    Footer.jsx, search.js and searchFilters.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+
+const Footer = () => {
+  return (
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.400">
+      2022 Dreamwork Real Estate, Inc.
+    </Box>
+  )
+ 
+}
+export default Footer
