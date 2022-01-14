23fa7de83badd1537835f284d1ff1b070a6c30e8
Turhan <turhan-ergene@hotmail.com>
2022-01-14 01:24:18 +0100

    Error: Objects are not valid as a React child (found: object with keys, will be fixed

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,27 +1,27 @@
 import { useContext } from 'react'
 import Image from 'next/image'
 import { Box, Icon, Flex } from '@chakra-ui/react'
-import { ScrollMenu, visibilityContext } from 'react-horizontal-scrolling-menu'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
 
 const LeftArrow = () => {
-  const { scrollPrev } = useContext(visibilityContext);
+  const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Flex justifyContent="center" alignItems="center" marginRight="1">
-      <Icon 
-        as ={FaArrowAltCircleLeft}
+    <Flex justifyContent='center' alignItems='center' marginRight='1'>
+      <Icon
+        as={FaArrowAltCircleLeft}
         onClick={() => scrollPrev()}
-        fontSize="2xl"
-        cursor="pointer"
+        fontSize='2xl'
+        cursor='pointer'
+
       />
     </Flex>
-  )
+  );
 }
 
 const RightArrow = () => {
-  const { scrollNext } = useContext(visibilityContext);
-
+  const { scrollNext } = useContext(VisibilityContext);
   return (
     <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon 
@@ -34,23 +34,24 @@ const RightArrow = () => {
   )
 }
 
-const ImageScrollbar = ({ data }) => {
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{overflow: 'hidden'}}>
-    {data.map((item)=>
-      <Box key={item.id} width="910px" itemId={item.id} overflow="hidden">
-        <Image 
-          alt="property"
-          placeholder="blur" 
-          blurDataUrl={item.url} 
-          src={item.url} 
-          width={1000} 
-          height={500}
-          sizes="(max-width:500px) 100px, (max-width):1023px, 400px, 1000px"
-        />
-        {item}
-      </Box>
-    )}
-  </ScrollMenu>
-}
+export default function ImageScrollbar ({ data }) {
 
-export default ImageScrollbar; 
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{overflow: 'hidden'}}>
+      {data.map((item)=> (
+        <Box key={item.id} width="910px" itemId={item.id} overflow="hidden">
+          <Image 
+            alt="property"
+            placeholder="blur" 
+            blurDataURL={item.url} 
+            src={item.url} 
+            width={1000} 
+            height={500}
+            sizes="(max-width:500px) 100px, (max-width):1023px, 400px, 1000px"
+          />
+          {item}
+        </Box>
+      ))}
+    </ScrollMenu>
+  )
+}
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,26 +1,42 @@
-import { Box, Flex, Spacer, Text } from '@chakra-ui/react'
-import { FaBed, FaBath } from 'react-icons/fa'
-import { BsGridFill } from 'react-icons/bs'
-import { GoVerified } from 'react-icons/go'
-import millify from 'millify'
+import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";
 
-import { baseUrl, fetchApi } from "../../utils/fetchApi"
-import ImageScrollbar from "../../components/ImageScrollbar"
+import { baseUrl, fetchApi } from "../../utils/fetchApi";
+import ImageScrollbar from "../../components/ImageScrollbar";
 
-
-const PropertDetails = ({ propertyDetails: { price, rentFrequency, room, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, photos} }) => (
+const PropertyDetails = ({
+  propertyDetails: {
+    price,
+    rentFrequency,
+    room,
+    title,
+    baths,
+    area,
+    agency,
+    isVerified,
+    description,
+    type,
+    purpose,
+    furnishingStatus,
+    photos,
+  },
+}) => (
   <Box maxWidth="1000px" margin="auto" p="4">
-    { photos && <ImageScrollbar data={photos}/>}
-
+    {photos && <ImageScrollbar data={photos} />}
   </Box>
-)
+);
 
-export async function getServerSideProps({ params: { id }}){ //*
-  const data = fetchApi(`${baseUrl}/properties/detail?ExternalIDs=${id}`)
+export default PropertyDetails;
 
-  return { 
+export async function getServerSideProps({ params: { id } }) {
+  const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+
+  return {
     props: {
-      propertyDetails: data
-    }
-  }
+      propertyDetails: data,
+    },
+  };
 }
